6ba04871ed7e15bfee6033e05fd56c80415402a7
dvanbeelen <34212264+dvbeelen@users.noreply.github.com>
2021-05-17 16:11:16 +0200

    Dissapeared walls now have no collisions

--- Assets/Dissapearing_Wall.gd
+++ Assets/Dissapearing_Wall.gd
@@ -1,10 +1,13 @@
 extends RigidBody
 
-export var frame = 1
+onready var col = $CollisionShape
 
+export var frame = 1
 
 func _process(delta):
 	if Global.red_frame == true:
 		self.visible = false
+		col.disabled = true
 	else:
 		self.visible = true
+		col.disabled = false
