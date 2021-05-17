d66ab22b50931d23109afb952bc037bdaa58eb12
dvanbeelen <34212264+dvbeelen@users.noreply.github.com>
2021-05-17 12:21:11 +0200

    Removed part of the game that will be implemented via plug-in

--- Global.gd
+++ Global.gd
@@ -1,9 +1,4 @@
 extends Node
 
-var textTriggerNr = 0
-
-var bookTexts = {
-	1 : 'The color is repellant, almost revolting ;', 
-	2 : 'a smouldering unclean yellow,',
-	3 : 'strangely faded by the slow-turning sunlight.'
-}
+func switch_frame():
+	pass
