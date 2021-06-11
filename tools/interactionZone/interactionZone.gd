f78372af6c039873f17c14aad17f07a5a06594e5
dvanbeelen <34212264+dvbeelen@users.noreply.github.com>
2021-06-11 13:23:54 +0200

    Fixed e-pressing during interactions

--- tools/interactionZone/interactionZone.gd
+++ tools/interactionZone/interactionZone.gd
@@ -3,6 +3,7 @@ extends Area
 export var interaction: String
 onready var chatScene = load("res://leechat/LEECHat.tscn")
 var inZone = false
+var interactableStarted = false
 
 func _on_interactionZone_body_entered(body):
 	if body.name == 'Player':
@@ -10,14 +11,16 @@ func _on_interactionZone_body_entered(body):
 		inZone = true
 
 func _process(delta):
-	if Input.is_action_just_pressed("interact") && inZone:
+	if Input.is_action_just_pressed("interact") && inZone && !interactableStarted:
 		match (interaction):
 			'':
 				pass
 			'chat':
+				interactableStarted = true
 				Global.openChat()
 
 func _on_interactionZone_body_exited(body):
 	if body.name == 'Player':
+		interactableStarted = false 
 		inZone = false
 		Global.hideInteractionPrompt()
